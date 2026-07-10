5498703dc37ec885e331ee0393be2e8c1d4dd152
harman <harmxn1320@gmail.com>
2026-07-10 10:59:05 +0530

    Changes in the style.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -60,21 +60,21 @@ export default function App() {
     }
   };
 
-  const useMyLocation = () => {
-    if (!navigator.geolocation) {
-      setError("Geolocation not supported");
-      return;
-    }
-    navigator.geolocation.getCurrentPosition(
-      (pos) => fetchByCoords(pos.coords.latitude, pos.coords.longitude),
-      () => setError("Unable to retrieve your location")
-    );
-  };
+  // const useMyLocation = () => {
+  //   if (!navigator.geolocation) {
+  //     setError("Geolocation not supported");
+  //     return;
+  //   }
+  //   navigator.geolocation.getCurrentPosition(
+  //     (pos) => fetchByCoords(pos.coords.latitude, pos.coords.longitude),
+  //     () => setError("Unable to retrieve your location")
+  //   );
+  // };
 
-  useEffect(() => {
-    useMyLocation();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   useMyLocation();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -116,7 +116,7 @@ export default function App() {
           <button type="submit" className="btn btn-primary">
             Search
           </button>
-          <button type="button" className="btn" onClick={useMyLocation}>
+          <button type="button" className="btn" id="button" onClick={useMyLocation}>
             My location
           </button>
         </form>
